a09fcffda25d98731392ec6954ae75ad0e811544
Nick Payne <nick@kurai.co.uk>
2011-02-07 17:54:31 +0000

    homebrewing bus to eradicate unnecessary jquery dependency

--- public/js/bus.js
+++ public/js/bus.js
@@ -4,16 +4,30 @@
 
 var Bus = {
     _bindTo: document,
+    listeners: {},
 
     publish: function(topic, data) {
         if (typeof data == "undefined") {
             data = null;
         }
-        $(Bus._bindTo).trigger(topic, data);
+
+        if (typeof this.listeners[topic] === "undefined") {
+            // no listeners for topic. cya
+            return;
+        }
+
+        var i = 0;
+        var listeners = this.listeners[topic];
+        for (var i = 0; i < listeners.length; i++) {
+            listeners[i](data);
+        }
     },
 
-    subscribe: function(topic, data, callback) {
-        $(Bus._bindTo).bind(topic, data, callback);
+    subscribe: function(topic, callback) {
+        if (typeof this.listeners[topic] === "undefined") {
+            this.listeners[topic] = [];
+        }
+        this.listeners[topic].push(callback);
     }
 }
         
